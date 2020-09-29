01e7717f4c3e83e018c1b607c9ceeda84e2fe672
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2020-09-29 00:06:08 -0300

    test of slack

--- src/tecnologia/tecnologia_controllers.js
+++ src/tecnologia/tecnologia_controllers.js
@@ -1,28 +1,28 @@
 const cenService = require('../services/centrales');
 
+const today = new Date();
+let yesterday = new Date();
+yesterday.setDate(today.getDate() - 1);
+yesterday.toISOString().split('T', 1)[0];
+console.log(yesterday);
+
+
 exports.getTech = async (req, res) => {
   console.log('[getTech][Request]' );
   try {
-    const genCentral = await cenService.getGenCentral("2020-09-22", 127);
+    const dataGenCentral = await cenService.getGenCentral("2020-09-25", 127);
+    let genCentral = 0;
+    for (let i = 0;  i < dataGenCentral.generacionBruta.length; i++) {
+      genCentral = genCentral + dataGenCentral.generacionBruta[i].generacion;
+    }
     console.log('[getTech][Res] ', genCentral);
-    res.status(200).json({data: data});
+    res.status(200).json({genCentral: genCentral});
   } catch (err) {
-    console.log('[getTech][Error] ', error);
+    console.log('[getTech][Error] ', err);
     res.status(500).send({err});
   }
 }
 
 
 
-// const promiseGetGenCentral = (idcentral) => {
-//   return new Promise((resolve, reject) => {
 
-//   })
-// }
-
-// let data;
-// await axios.get(`${URL_central}${params.idcentral}.json/?auth_key=${AUTH_KEY}`, { params: params }).then(result => {
-//   data = result.data;
-// }).catch(err => {
-//   console.log(err.code);
-// });
